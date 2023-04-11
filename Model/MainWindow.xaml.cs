93e569ee0184298578610cead6a4dd4d018011aa
Ksu <ksysha.2002r@gmail.com>
2023-04-11 12:50:48 +0300

    change textbox properties

--- Model/MainWindow.xaml.cs
+++ Model/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ModelPolosin;
 
@@ -7,6 +9,11 @@ namespace ModelPolosin;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+    {
+        Regex regex = new Regex("[^0-9.-]+");
+        e.Handled = regex.IsMatch(e.Text);
+    }
     public MainWindow()
     {
         InitializeComponent();
