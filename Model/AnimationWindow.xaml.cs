84b8887572ae7fc9fd0412dedaf8afeabfa015a0
Оля Бездудная <lyolya0606@gmail.com>
2023-06-09 13:01:53 +0300

    осталось немного...

--- Model/AnimationWindow.xaml.cs
+++ Model/AnimationWindow.xaml.cs
@@ -93,6 +93,12 @@ public partial class AnimationWindow : Window {
     private double _coverVelocity;
     private static int _count;
     private int _countOfRows;
+    private const int _maxHeight = 325;
+    private const int _maxLength = 778;
+    private const int _minHeight = 84;
+    private const int _minLength = 100;
+    private int _canvasWidth;
+    
         
     public class Ball {
         public Ellipse Shape { get; set; }
@@ -110,38 +116,55 @@ public partial class AnimationWindow : Window {
     
     public AnimationWindow(double height, double length, double coverVelocity) {
         InitializeComponent();
-        _height = height * 10000;
-         // _length = length * 100;
-        _length = 650;
+        _height = height * 8000;
+         _length = length * 100;
+        //_length = 650;
         _coverVelocity = coverVelocity;
+
+        if (_height > _maxHeight) {
+            _height = _maxHeight;
+        }
+        
+        if (_length > _maxLength) {
+            _length = _maxLength;
+        }
+        
+        if (_height < _minHeight) {
+            _height = _minHeight;
+        }
+        
+        if (_length < _minLength) {
+            _length = _minLength;
+        }
+        
         HeightTextBox.Text = height.ToString();
         LengthTextBox.Text = length.ToString();
         CoverVelocityTextBox.Text = coverVelocity.ToString();
         
         _count = (int)(_length / 19);
-        if (_count % 2 == 0) {
-            _length = _count * 19 + 6;
-        } else {
-            _length = _count * 19 + 9;
-        }
+        // if (_count % 2 == 0) {
+        //     _length = _count * 19 + 9;
+        // } else {
+        //     _length = _count * 19 + 9;
+        // }
 
         // if (_count % 2 != 0) {
         //     _count -= 1;
         // }
         _countOfRows = (int)_height / 21;
-        
-        MyCanvas.Height = _height;
-        MyCanvas.Width = _length;
-        MyCanvas.Background = Brushes.Transparent;
-        CanvasBorder.BorderBrush = Brushes.Black;
-        CanvasBorder.Height = _height;
-        CanvasBorder.Width = _length;
         DrawBalls();
         
         
     }
 
     private void DrawBalls() {
+        MyCanvas.Height = _height;
+        //MyCanvas.Width = _length;
+        MyCanvas.Background = Brushes.Transparent;
+        CanvasBorder.BorderBrush = Brushes.Black;
+        CanvasBorder.Height = _height;
+        //CanvasBorder.Width = _length;
+        
         balls = new BallMovement[_count * _countOfRows];
         int k = 0;
         for (int j = 0; j < _countOfRows; j++) {
@@ -151,17 +174,47 @@ public partial class AnimationWindow : Window {
                 .WithProperty(Shape.FillProperty, i => new SolidColorBrush(Color.FromArgb(255, 135, 206, 250)))
                 .WithXY(x => x * 20.0,
                     y => y * 0.0 + j * 20.0 - 23.0);
-                
-                
+            if (_count > 20) {
+                MyCanvas.Width = _count * 19 + 10;
+                CanvasBorder.Width = _count * 19 + 10;
+            } else if (_count == 41) {
+                MyCanvas.Width = _count * 19 + 12;
+                CanvasBorder.Width = _count * 19 + 12;
+            } else {
+                MyCanvas.Width = (_count - 1) * 19 + 7;
+                CanvasBorder.Width = (_count - 1) * 19 + 7;
+            }
+            
+            if (_count == 5) {
+                MyCanvas.Width = (_count - 1) * 19;
+                CanvasBorder.Width = (_count - 1) * 19;
+            }
+            if (_count == 19) {
+                MyCanvas.Width = _count * 19 + 2;
+                CanvasBorder.Width = _count * 19 + 2;
+            }
+
+            if (_count == 18 || _count == 19) {
+                MyCanvas.Width = _count * 19 - 7;
+                CanvasBorder.Width = _count * 19 - 7;
+            }
+            
+            if ((_count >= 27 && _count <= 30)) {
+                MyCanvas.Width = _count * 19;
+                CanvasBorder.Width = _count * 19;
+            }
+            
+            if ((_count >= 20 && _count <= 26)) {
+                MyCanvas.Width = _count * 19 - 3;
+                CanvasBorder.Width = _count * 19 - 3;
+            }
+
             int widthBetween = -60;
             foreach (var ellipse in circles) {
-                //MyCanvas.Children.Add(ellipse);
      
                 balls[k] = new BallMovement() {
                     Ball = new Ball() { Shape = ellipse, Position = new Point(widthBetween, j * 20.0 - 16.0), Radius = 5 },
-                    /*Speed = 1 + j,*/
                     Speed = _coverVelocity / _height * (_height - j * 20),
-                    //Angle = 45 // задаем направление движения шарика в градусах
                 };
                 Canvas.SetLeft(balls[k].Ball.Shape, widthBetween);
                 MyCanvas.Children.Add(balls[k].Ball.Shape);
@@ -174,7 +227,7 @@ public partial class AnimationWindow : Window {
     private Storyboard myStoryboard;
     
     private void MakeAnimation(BallMovement ball, bool isStarted = false) {
-        var startPositionX = isStarted ? ball.Ball.Position.X : -58;
+        var startPositionX = isStarted ? ball.Ball.Position.X : -57;
         var myDoubleAnimation = new DoubleAnimation {
                 
             From = startPositionX,
@@ -199,27 +252,25 @@ public partial class AnimationWindow : Window {
         
     private void ButtonDo_Click(object sender, RoutedEventArgs e) {
         if (ButtonDo.Content.ToString() == "Run") {
+            if (!CheckFields()) {
+                return;
+            }
             ButtonDo.Content = "Stop";
-            for (int i = 0; i < _count * (_countOfRows - 1); i++) {
-                MakeAnimation(balls[i], true);
+            try {
+                for (int i = 0; i < _count * (_countOfRows - 1); i++) {
+                    MakeAnimation(balls[i], true);
+                }
+            } catch (Exception) {
+                MessageBox.Show("Animation Error!");
             }
         } else {
             ButtonDo.Content = "Run";
-            
             MyCanvas.Children.Clear();
             for (int i = 0; i < _count * (_countOfRows - 1); i++) {
-                
                 myStoryboard.Remove(balls[i].Ball.Shape);
             }
-            // foreach (var ball in balls) {
-            //     myStoryboard.Remove(ball.Ball.Shape);
-            // }
-          
-           DrawBalls();
+            DrawBalls();
         }
-        // foreach (var ball in balls) {
-        //     MakeAnimation(ball, true);
-        // }
 
     }
     private readonly List<string> _incorrectValues = new();
@@ -246,4 +297,76 @@ public partial class AnimationWindow : Window {
                 _incorrectValues.Remove(textBox.Name);
         }
     }
+
+    private bool CheckFields() {
+        if (!CheckTextBox) {
+            MessageBox.Show("Fix highlighted red fields!");
+            return false;
+        }
+
+        if (LengthTextBox.Text == "0" || HeightTextBox.Text == "0" || CoverVelocityTextBox.Text == "0") {
+            MessageBox.Show("Values cannot be zero");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void ButtonApply_Click(object sender, RoutedEventArgs e) {
+        if (!CheckFields()) {
+            return;
+        }
+
+        ButtonDo.Content = "Run";
+        MyCanvas.Children.Clear();
+        // for (int i = 0; i < _count * (_countOfRows - 1); i++) {
+        //         
+        //     myStoryboard.Remove(balls[i].Ball.Shape);
+        // }
+
+        _height = double.Parse(HeightTextBox.Text) * 8000;
+        _length = double.Parse(LengthTextBox.Text) * 100;
+        _coverVelocity = double.Parse(CoverVelocityTextBox.Text);
+        
+        if (_height > _maxHeight) {
+            _height = _maxHeight;
+        }
+        
+        if (_length > _maxLength) {
+            _length = _maxLength;
+        }
+        
+        if (_height < _minHeight) {
+            _height = _minHeight;
+        }
+        
+        if (_length < _minLength) {
+            _length = _minLength;
+        }
+        
+        _count = (int)(_length / 19);
+
+        // if (Math.Round(_length / 19, 3) - _count >= 0.5) {
+        //     _length = _count * 19 + 10;
+        // } else {
+        //     _length = _count * 19 + 6;
+        // }
+        
+        // if (_count % 2 == 0) {
+        //     _length = _count * 19 + 9;
+        // } else {
+        //     _length = _count * 19 + 9;
+        // }
+
+        // if (_count % 2 != 0) {
+        //     _count -= 1;
+        // }
+        _countOfRows = (int)_height / 21;
+        
+        
+       
+        DrawBalls();
+        
+    }
+    
 }
